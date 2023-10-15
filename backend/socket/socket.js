00234ab83a813c80d4c476ff3e65a559e8432a92
Sasidhar Veerni <81133052+Sasidharveerni@users.noreply.github.com>
2023-10-15 13:33:03 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "https://threads-d5hh.vercel.app/",
 		methods: ["GET", "POST"],
 	},
 });
