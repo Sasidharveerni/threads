40b030717ac3b9245d2bcad966f43781410d2c4c
Sasidhar Veerni <81133052+Sasidharveerni@users.noreply.github.com>
2023-10-15 13:35:19 +0530

    Update and rename server.js to index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -30,4 +30,4 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+module.exports = app;
